f83eb921721247508d6190edcf63cb87f5749029
Rosie Wood <rwood@turing.ac.uk>
2023-06-15 11:21:13 +0100

    add fall over script

--- Assets/Scripts/FallDetector.cs
+++ Assets/Scripts/FallDetector.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FallDetector : MonoBehaviour
 {
     public GameObject head;
     public float threshold = 1;
+
+    private Vector3 startPosition;
+    private float totalDistance;
+    
     // Start is called before the first frame update
-    private Vector3 startposition;
     void Start()
     {
-        startposition = transform.position; 
+        startPosition = transform.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float headheight = head.transform.position.y;
-        if (headheight < threshold)
+        float distance = Vector3.Distance( 
+            startPosition, transform.position 
+            );
+        totalDistance += distance;
+        startPosition = transform.position;
+        print(totalDistance);
+        
+        float headHeight = head.transform.position.y;
+        if (headHeight < threshold)
         {
-            print("fell over");
+            print("Fell over");
+            print("Final distance: "+totalDistance);
+            SceneManager.LoadScene( 
+                SceneManager.GetActiveScene().buildIndex 
+                );
         }
+        
     }
 }
