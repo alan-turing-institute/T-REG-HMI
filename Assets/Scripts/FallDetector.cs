3348743bc2bc7c0ff3fe83207070eac4cbebcde3
Oliver Strickson <ostrickson@turing.ac.uk>
2023-06-15 16:27:03 +0100

    Fall detector write distance/status to text displays

--- Assets/Scripts/FallDetector.cs
+++ Assets/Scripts/FallDetector.cs
@@ -13,11 +13,20 @@ public class FallDetector : MonoBehaviour
     private Vector3 spinePosition;
     private float totalDistance;
     
+    private TextDisplay display;
+
+    public float getTotalDistance()
+    {
+        return totalDistance;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         lowerSpine = GameObject.Find("lower_spine");
         spinePosition = lowerSpine.transform.position;
+
+        display = GameObject.Find("Canvas").GetComponent<TextDisplay>();
     }
 
     // Update is called once per frame
@@ -29,14 +38,15 @@ public class FallDetector : MonoBehaviour
         totalDistance += distance;
         spinePosition = lowerSpine.transform.position;
         
+        display.Info(string.Format("Distance: {0:N1} m", totalDistance / 5.0));
+
         float headHeight = head.transform.position.y;
         if (headHeight < threshold)
         {
-            print("Fell over");
-            print("Final distance: "+totalDistance);
+            display.Announce("Fell over!");
             SceneManager.LoadScene( 
                 SceneManager.GetActiveScene().buildIndex 
-                );
+            );
         }
         
     }
